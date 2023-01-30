2d2b7121dd1c611363225c42998cdbb40299e58f
오현주 <hyunjoooh@ohyeonjuui-MacBookPro.local>
2023-01-30 17:14:53 +0900

    Add: 초기세팅 완료  /  Modify: 공통 브랜치 develop 빼고 삭제

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,5 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import AppleSDGothicNeoL from '../asset/fonts/AppleSDGothicNeoL.ttf';
+import AppleSDGothicNeoR from '../asset/fonts/AppleSDGothicNeoR.ttf';
+import AppleSDGothicNeoM from '../asset/fonts/AppleSDGothicNeoM.ttf';
+import AppleSDGothicNeoB from '../asset/fonts/AppleSDGothicNeoB.ttf';
+import NotoSansThin from '../asset/fonts/NotoSans-Thin.ttf';
+import NotoSansLight from '../asset/fonts/NotoSans-Light.ttf';
+import NotoSansRegular from '../asset/fonts/NotoSans-Regular.ttf';
+import NotoSansMedium from '../asset/fonts/NotoSans-Medium.ttf';
+import NotoSansSemibold from '../asset/fonts/NotoSans-SemiBold.ttf';
+import NotoSansBold from '../asset/fonts/NotoSans-Bold.ttf';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
@@ -12,83 +22,76 @@ const GlobalStyle = createGlobalStyle`
 
   @font-face {
     font-family: 'AppleSDGothicNeo';
-    src: local('../asset/fonts/AppleSDGothicNeoL.ttf' format('ttf'));
+    src: url(${AppleSDGothicNeoL}) format('ttf');
     font-weight: 300;
     font-display: swap;
   }
 
   @font-face {
     font-family: 'AppleSDGothicNeo';
-    src: local('../asset/fonts/AppleSDGothicNeoR.ttf' format('ttf'));
+    src: url(${AppleSDGothicNeoR}) format('ttf');
     font-weight: 400;
     font-display: swap;
   }
 
   @font-face {
     font-family: 'AppleSDGothicNeo';
-    src: local('../asset/fonts/AppleSDGothicNeoM.ttf' format('ttf'));
+    src: url(${AppleSDGothicNeoM}) format('ttf');
     font-weight: 500;
     font-display: swap;
   }
 
   @font-face {
     font-family: 'AppleSDGothicNeo';
-    src: local('../asset/fonts/AppleSDGothicNeoB.ttf' format('ttf'));
+    src: url(${AppleSDGothicNeoB}) format('ttf');
     font-weight: 600;
     font-display: swap;
   }
 
   @font-face {
     font-family: 'NotoSans';
-    src: local('../asset/fonts/NotoSans-Thin.ttf' format('ttf'));
+    src: url(${NotoSansThin}) format('ttf');
     font-weight: 300;
     font-display: swap;
   }
 
   @font-face {
     font-family: 'NotoSans';
-    src: local('../asset/fonts/NotoSans-Light.ttf' format('ttf'));
+    src: url(${NotoSansLight}) format('ttf');
     font-weight: 400;
     font-display: swap;
   }
 
   @font-face {
     font-family: 'NotoSans';
-    src: local('../asset/fonts/NotoSans-Regular.ttf' format('ttf'));
+    src: url(${NotoSansRegular}) format('ttf');
     font-weight: 500;
     font-display: swap;
   }  
 
   @font-face {
     font-family: 'NotoSans';
-    src: local('../asset/fonts/NotoSans-Medium.ttf' format('ttf'));
+    src: url(${NotoSansMedium}) format('ttf');
     font-weight: 600;
     font-display: swap;
   } 
 
   @font-face {
     font-family: 'NotoSans';
-    src: local('../asset/fonts/NotoSans-SemiBold.ttf' format('ttf'));
+    src: url(${NotoSansSemibold}) format('ttf');
     font-weight: 600;
     font-display: swap;
   } 
 
   @font-face {
     font-family: 'NotoSans';
-    src: local('../asset/fonts/NotoSans-Bold.ttf' format('ttf'));
-    font-weight: 700;
-    font-display: swap;
-  } 
-
-  @font-face {
-    font-family: 'testFont';
-    src: url('../asset/fonts/Unbounded-SemiBold.ttf');
+    src: url(${NotoSansBold}) format('ttf');
     font-weight: 700;
     font-display: swap;
   } 
 
   body {
-    font-family: 'testFont';
+    font-family: 'AppleSDGothicNeo';
     line-height: 1.5;
   }
 `;
