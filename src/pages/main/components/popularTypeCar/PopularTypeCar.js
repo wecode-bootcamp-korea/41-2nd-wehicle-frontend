6ab1a0ed1dd47b4f505f86da8c4d814a1ffc1a2d
오현주 <hyunjoooh@ohyeonjuui-MacBookPro.local>
2023-01-30 18:10:21 +0900

    Add: 메인 레이아웃 및 기능 구현 완료

---
+++ src/pages/main/components/popularTypeCar/PopularTypeCar.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import PopularTypeCarContent from './PopularTypeCarContent';
+
+function PopularTypeCar() {
+  const [currentTab, setCurrentTab] = useState('Sedan');
+  const [typeProduct, setTypeProduct] = useState([]);
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+  const changeUnderLine = e => {
+    setCurrentTab(e.target.value);
+  };
+
+  useEffect(() => {
+    fetch(
+      `${BASE_URL}/cars?type=${currentTab === 'Sedan' ? 1 : 2}&offset=0&limit=6`
+    )
+      .then(res => res.json())
+      .then(data => {
+        setTypeProduct(data.data);
+      });
+  }, [currentTab]);
+
+  return (
+    <PopularTypeCarBox>
+      <Title>What Kind of</Title>
+      <CategoryBox>
+        <BtnSedan title={currentTab} value="Sedan" onClick={changeUnderLine}>
+          Sedan
+        </BtnSedan>
+        <BtnSuv title={currentTab} value="SUV" onClick={changeUnderLine}>
+          SUV
+        </BtnSuv>
+      </CategoryBox>
+      <PopularTypeCarContainer>
+        {typeProduct.map(data => {
+          return <PopularTypeCarContent key={data.id} data={data} />;
+        })}
+      </PopularTypeCarContainer>
+    </PopularTypeCarBox>
+  );
+}
+
+export default PopularTypeCar;
+
+const PopularTypeCarBox = styled.div`
+  display: block;
+  margin: 110px 100px 110px;
+`;
+
+const Title = styled.h1`
+  font-size: 40px;
+  font-weight: 700;
+`;
+
+const CategoryBox = styled.div`
+  width: 100%;
+  margin-top: 30px;
+  border-bottom: 1px solid #999;
+`;
+
+const BtnSuv = styled.button`
+  display: inline-block;
+  width: 150px;
+  padding: 10px;
+  border: none;
+  background: #fff;
+
+  text-align: center;
+  font-size: 24px;
+  font-weight: 500;
+
+  cursor: pointer;
+  border-bottom: ${props => (props.title === 'SUV' ? '4px solid #222' : '')};
+`;
+
+const BtnSedan = styled.button`
+  display: inline-block;
+  width: 150px;
+  padding: 10px;
+  border: none;
+  background: #fff;
+
+  text-align: center;
+  font-size: 24px;
+  font-weight: 500;
+
+  cursor: pointer;
+  border-bottom: ${props => (props.title === 'Sedan' ? '4px solid #222' : '')};
+`;
+
+const PopularTypeCarContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 400px);
+  column-gap: 30px;
+  justify-content: center;
+
+  padding: 40px 0;
+`;
