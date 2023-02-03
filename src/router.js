7984a682fa2673c4e87502a70c962cdb7c808cfd
오현주 <hyunjoooh@ohyeonjuui-MacBookPro.local>
2023-02-03 11:29:56 +0900

    Add: 입찰 모달창 레이아웃 구현 완료

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import NotFound from './pages/notFound/NotFound';
 import ProductDetail from './pages/productDetail/ProductDetail';
 import ProductList from './pages/productList/ProductList';
 import KakaoRedirect from './pages/login/KakaoRedirect';
+import BidModal from './pages/productDetail/bidModal/BidModal';
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
       { path: '/auth/kakao/callback', element: <KakaoRedirect /> },
       { path: '/productDetail', element: <ProductDetail /> },
       { path: '/productList', element: <ProductList /> },
+      { path: '/bidModal', element: <BidModal /> },
     ],
   },
 ]);
